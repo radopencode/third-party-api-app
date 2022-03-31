910abcbeddd0b44718eee0abdbdd45b443cfc612
Ryan Day <radday1@gmail.com>
2022-03-31 15:05:17 -0700

    Adds to controller for API url

--- app/controllers/quote_controller.rb
+++ app/controllers/quote_controller.rb
@@ -1,2 +1,7 @@
 class QuoteController < ApplicationController
+  def index
+    request = HTTP.get("https://quoteclear.web.app/api/random")
+    request = request.parse(:json)
+    render json: request.as_json 
+  end
 end
